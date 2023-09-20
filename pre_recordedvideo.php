02ca185168bd6a04defb9ace3dbe7fae58a4ee54
kazi <hasan.idb7@gmail.com>
2023-09-20 21:29:03 +0600

    Module 3 assignment

---
+++ pre_recordedvideo.php
@@ -0,0 +1,26 @@
+<?php
+// Introduction to arrays
+
+// $n="12";
+// $students= array("Rahim","Karim","Jasim","Rina","Bina",900);
+// $n=count($students);
+// //Ascending 
+// for($i=0;$i<$n; $i++){
+//     echo $i. " . ". $students[$i]. "\n";
+// }
+
+// //Descending 
+
+// for ($i=$n-1;$i>=0; $i--){
+//     echo $i. " . ". $students[$i]. "\n";
+// }
+
+//Array Manipulation
+
+
+// $fruits_list = array(0 => 'Orange', 1=> 'Apple', 2 => 'Banana',3 => 'Cherry');
+// $result= array_shift($fruits_list);
+// print_r($fruits_list);
+// echo '</br>'.$result;
+
+function test($x = 5) { return $x * $x; } echo test();
