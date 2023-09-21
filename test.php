62bfd207df259abbd7c311c77f40e37cc2547758
Imam Hasan Kazi <ihk2017@users.noreply.github.com>
2023-09-21 13:39:29 +0600

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+function celsius_to_fahrenheit($celsius) {
+    $fahrenheit = ($celsius * 9/5) + 32;
+    return $fahrenheit;
+}
+
+// Input temperature in Celsius
+$celsius = 32; // Replace with your desired temperature in Celsius
+
+// Convert Celsius to Fahrenheit using the function
+$fahrenheit = celsius_to_fahrenheit($celsius);
+
+// Display the result
+echo "Temperature in Fahrenheit: " . $fahrenheit;
