656257aedb30f16a5225b4ee4a4d5dbe132fe007
kazi <hasan.idb7@gmail.com>
2023-09-25 12:28:40 +0600

    update

--- test.php
+++ test.php
@@ -1,14 +1,5 @@
 <?php
-$array = [3, 8, 1, 12, 5,88, 9];
-function getMaxVal($x){
-    $max=$x[0];
-    foreach($x as $value){
-        if($value> $max){
-            $max=$value;
-        }
-    }
-    return $max;
-}
-$result =getMaxVal($array);
-echo $result;
+$numbers = [1, 2, 3, 4, 5];
 
+ print_r($numbers);
+ var_dump($numbers);
