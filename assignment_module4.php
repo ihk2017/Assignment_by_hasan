cb42004ae5063cf28b3e471a12cad568a3ac5bdb
kazi <hasan.idb7@gmail.com>
2023-10-02 20:18:11 +0600

    updated

--- assignment_module4.php
+++ assignment_module4.php
@@ -5,7 +5,7 @@ class Product {
     private $name;
     private $price;
 
-    public function __construct($id, $name, $price) {
+    public function __construct(int $id, string $name, float $price) {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
