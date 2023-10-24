45756dc1fbc1dba880b24b6252b55c00ec565be2
kazi <hasan.idb7@gmail.com>
2023-10-24 15:36:10 +0600

    assignment_module5

--- app_manage/menu.php
+++ app_manage/menu.php
@@ -5,10 +5,10 @@
 
            </div> 
            <div class="menus text-start">
+                <h6><a href="index.php">Home</a></h6>
                 <h6><a href="registration.php">Registration</a></h6>
                 <h6><a href="login.php">Login</a</h6><br/><br/>
-                <h6><a href="role_managementaccess.php">Role Management</a></h6><br/>
-                
+                <h6><a href="role_managementaccess.php">Role Management</a></h6><br/>  
            </div>
             
         </div>
